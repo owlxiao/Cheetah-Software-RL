92a71be5a03b073c5e937a0e069bbd285c8cb272
Xiao <79354981+owlxiao@users.noreply.github.com>
2023-05-08 17:24:40 +0800

    Feat: Upload the program by hand

--- scripts/send_to_mini_cheetah.sh
+++ scripts/send_to_mini_cheetah.sh
@@ -20,12 +20,12 @@ cp -r ../config robot-software
 DATE=$(date +"%Y%m%d%H%M")
 #scp -r robot-software user@10.0.0.34:~/robot-software-$DATE/
 
-if [ -z "$2" ]
-then
-  echo "No mini-cheetah number specified, using old mini-cheetah address"
-  scp -r robot-software user@10.0.0.34:~/
-else
-  scp -r robot-software user@10.0.0.4$2:~/
-fi
+# if [ -z "$2" ]
+# then
+#   echo "No mini-cheetah number specified, using old mini-cheetah address"
+#   scp -r robot-software user@10.0.0.34:~/
+# else
+#   scp -r robot-software user@10.0.0.4$2:~/
+# fi
 
 
